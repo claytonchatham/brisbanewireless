3a56684aca54ccd2481a7019e8c806578877aadd
Clayton Chatham <claytonchatham@gmail.com>
2013-07-25 07:31:47 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brisbanewireless::Application.config.secret_token = '1a140601e787ac9366e4e0358dd90a137e09b9e59be35c53ab2b9e593b7cf9d4d6c27fcc28c28a99e42f45e794b4832daff9c684c7ea3bcba4e3f694231868d4'
